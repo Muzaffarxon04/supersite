0a68c7ed1cbbd1b740f53275da180a5f1e6a0ce5
Muzaffarxon04 <muzaffarxonabdusalomov04@gmail.com>
2022-04-04 14:01:24 +0500

    add animation swipe

--- js/main.js
+++ js/main.js
@@ -2,6 +2,53 @@ let searchInput = document.querySelector(".search-input")
 let searchWrapper = document.querySelector(".search-wrapper")
 let searchIcon = document.querySelector(".search-icon")
 let Item = document.querySelectorAll(".sidebar__list")
+const elForm = document.querySelector('.contact-form');
+const serForm = document.querySelector('.servises__form');
+
+const n = document.querySelector('#name')
+const eml = document.querySelector('#email')
+const c = document.querySelector('#company')
+const t = document.querySelector('#phone')
+
+
+
+
+const elEmailInput = document.querySelector('.email-input');
+const elPasswordInput = document.querySelector('.password-input');
+
+
+serForm.addEventListener('submit', e=>{
+    e.preventDefault();
+    
+    const Name = serForm[0].value;
+    const Email = serForm[1].value;
+    const Company = serForm[2].value;
+    const Phone = serForm[3].value;
+    
+    fetch('https://supersiteuz.herokuapp.com', {
+    method: "POST",
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name: Name, 
+        email: Email,
+        company: Company,
+        email: Phone
+    })
+}).then(res => res.json()).then(data => {
+    if(data){
+       alert(data)
+    }else{
+      console.log("error");
+    }
+})
+})
+
+
+
+
+
 
 Item.forEach(e => {
 e.addEventListener("click", (u) => {
@@ -33,3 +80,7 @@ function w3_open() {
 
 
 
+
+  
+
+  
